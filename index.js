fe57c1ae2265d482241b65f23cf7c9d2f7054ac9
habtamusisay573-wq <habtamusisay573@gmail.com>
2026-01-23 11:12:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -144,5 +144,5 @@ function startDrawingNumbers(existingDrawn) {
         do { n = Math.floor(Math.random() * 75) + 1; } while(drawn.includes(n));
         drawn.push(n);
         db.ref('game/drawn').set(drawn);
-    }, 4000);
+    }, 2000);
 }
